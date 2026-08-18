0d16d7647fe52eebbaa062a3e4c9786e7f19e374
williamNs <williamtrybe@gmail.com>
2026-08-18 17:26:43 -0300

    Criando o componente da tela principal

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,14 @@
+import './StartScreen.css';
+
+
+function StartScreen() {
+  return (
+    <div className="start_page">
+        <h2>Secret Word</h2>
+        <p>Clique no botão para começar o jogo!</p>
+        <button>Começar o Jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen;
