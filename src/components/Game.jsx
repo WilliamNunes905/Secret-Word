d82b1bf771f10db4fee7a7e584d19baf1880a051
williamNs <williamtrybe@gmail.com>
2026-08-19 18:52:49 -0300

    feat: Implementando a tela de Game e sua estilização

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,8 +2,26 @@ import './Game.css';
 
 function Game({ verifyLetter }) {
   return (
-    <div>
-      <button onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className="game">
+      <p className="points">Pontuação: 000</p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">Dica sobre a palavra: <span>Dica</span></h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type='text' name='letter' maxLength='1' />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a</span>
+        <span>b</span>
+      </div>
     </div>
   )
 }
