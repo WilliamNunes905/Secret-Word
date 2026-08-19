02a4d33882dd54f82b829d10a8b0aac129ff98b9
William Nunes <williamtrybe@gmail.com>
2026-08-19 15:23:30 -0300

    Criando os componentes para iniciar o jogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css';
+import { useState } from 'react';
+
+import { wordsList } from './data/wordsData';
 
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 
 
 function App() {
+  const [words] = useState(wordsList);
+  
+  const stages = [
+    {id: 1, name: "start"},
+    {id: 2, name: "game"},
+    {id: 3, name: "end"}
+  ];
+  
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  console.log(words);
+  
+
+  
   return (
     <div>
-      <StartScreen />
+      { gameStage === "start" && <StartScreen /> }
+      { gameStage === "game" && <Game /> }
+      { gameStage === "end" && <GameOver /> }
     </div>
   )
 }
