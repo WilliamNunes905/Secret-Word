56a46c65d65a3d472edf08e74a45393f12a18252
williamNs <williamtrybe@gmail.com>
2026-08-21 15:13:04 -0300

    feat: Finalizando a tela de gameOver

--- src/App.jsx
+++ src/App.jsx
@@ -98,7 +98,11 @@ function App() {
        guesses={guesses}
        score={score}
       /> }
-      { gameStage === "end" && <GameOver retry={retry} /> }
+      { gameStage === "end" && <GameOver
+      retry={retry}
+      score={score}
+      />
+      }
     </div>
   )
 }
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,10 +1,12 @@
 import './GameOver.css'
 
-function GameOver({ retry }) {
+function GameOver({ retry, score }) {
   return (
     <div>
       <h1>Fim de Jogo</h1>
-      <h2>Você perdeu! Tente novamente.</h2>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>
+      </h2>
       <button onClick={retry}>Reiniciar Jogo</button>
     </div>
   )
